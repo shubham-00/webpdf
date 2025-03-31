1d4ea58d32eaf0559dffc989c69cd16a4cb783bf
shubham-00 <srpatel980@gmail.com>
2025-03-30 22:59:59 -0400

    194

--- script.js
+++ script.js
@@ -190,7 +190,8 @@ async function chooseCamera(selectedDeviceId) {
 	// Set constraints for the video stream
 	const constraints = {
 		video: {
-			deviceId: selectedDeviceId ? { exact: selectedDeviceId } : undefined,
+			// deviceId: selectedDeviceId ? { exact: selectedDeviceId } : undefined,
+			deviceId: selectedDeviceId,
 			// width: { ideal: 1280 }, // Set ideal width
 			// height: { ideal: 720 }, // Set ideal height
 		},
